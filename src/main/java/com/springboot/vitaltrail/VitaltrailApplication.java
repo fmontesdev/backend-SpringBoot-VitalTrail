cbcd2032f0a1d8279ce7d8bd5623a958d0b4d54b
fmontesdev <f.montesdoria@gmail.com>
2025-03-03 07:04:34 +0100

    Estructura de proyecto y configuración con Spring Boot v3.4.3, Maven, JPA, y PostgreSQL

---
+++ src/main/java/com/springboot/vitaltrail/VitaltrailApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.vitaltrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+// @EnableJpaRepositories("com.springboot.entrename")
+public class VitaltrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaltrailApplication.class, args);
+	}
+
+}
